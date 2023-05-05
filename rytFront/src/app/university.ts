fb896035841d26588a28fdad21c1ab252702e0b9
Damirrrrr <amanzholovdamir@inbox.ru>
2023-05-06 00:13:22 +0600

    Uni profile's

--- rytFront/src/app/university.ts
+++ rytFront/src/app/university.ts
@@ -31,8 +31,15 @@ export const universities = [
   {
     id: 4,
     name: 'KAZNU',
-    description: 'Suleiman Demirel University was opened in 1996 with the participation of the First President of Kazakhstan – Elbasy Nursultan Nazarbayev and the 9th President of Turkey – Suleiman Demirel. The main goal of the Strategic Development Plan of the University is to become one of the best international universities in the country and throughout Central Asia.',
+    description: "KazNU is the oldest classical university of the Republic established by a Decree of the Kazakh Regional Committee (KRC) office dated November 13, 1933. One year after Kazakhstan's 1990 declaration of independence, the name was changed to Al-Farabi Kazakh State University. According to the QS World University Rankings 2021 KazNU takes 165th place in the rating of the best universities of the world.",
     img: "https://www.kaznu.kg/assets/images/logonew.png",
     url: "https://www.kaznu.kz/ru"
+  },
+  {
+    id: 5,
+    name: 'Astana IT',
+    description: 'Astana IT University is the leader of innovative continuous IT education and science with stable academic traditions and high social responsibility',
+    img: "https://astanait.edu.kz/wp-content/uploads/2020/05/aitu-logo-3.png",
+    url: "https://astanait.edu.kz/"
   }
 ];
